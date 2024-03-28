54402653d13b452d5eca4cb85125417b65009a0d
Yunus Emre SERT <yunusemresert19@gmail.com>
2024-03-28 22:09:46 +0300

    Complete the first stage

--- index.js
+++ index.js
@@ -6,12 +6,30 @@ const port = 3000;
 app.use(express.static("public"));
 app.use(express.urlencoded( { extended: true } ));
 
+let allTitles = [];
+let allPosts = [];
+
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+    res.render("index.ejs", {titles: allTitles});
 });
 
 app.get("/add-a-post", (req, res) => {
     res.render("add-post.ejs");
+});
+
+app.get("/posts/:id", (req, res) => {
+    res.send(`<h1>${allTitles[req.params.id]}</h1><br><br>${allPosts[req.params.id]}`);
+});
+
+app.post("/submit", (req, res) => {
+    if (req.body["title"] && req.body["post"]) {
+        allTitles.push(req.body["title"]);
+        allPosts.push(req.body["post"]);
+        res.redirect("/");
+    }
+    else {
+        res.sendStatus(404);
+    }
 });
 
 app.listen(port, () => {
